d768c1fd48d0d3397e17cbc8cff53260d6658e14
Yakimory <yastrebdev@gmail.com>
2024-06-17 18:16:07 +0700

    first commit

---
+++ client/script.js
@@ -0,0 +1,76 @@
+const messageElement = document.querySelector(".message");
+const cellElements = document.querySelectorAll(".cell");
+
+let field = ["", "", "", "", "", "", "", "", ""];
+
+let symbol = null;
+let turn = null;
+let isGameActive = false;
+
+let ws = new WebSocket("ws://localhost:8080");
+
+ws.onmessage = message => {
+    const response = JSON.parse(message.data);
+    
+    if (response.method === "join") {
+        symbol = response.symbol;
+        turn = response.turn;
+        isGameActive = symbol === turn;
+        updateMessage();
+    }
+
+    if (response.method === "update") {
+        field = response.field;
+        turn = response.turn;
+        isGameActive = symbol === turn;
+        updateBoard();
+        updateMessage();
+    }
+
+    if (response.method === "result") {
+        field = response.field;
+        updateBoard();
+        isGameActive = false;
+        setTimeout(() => {
+            messageElement.textContent = response.message;
+        }, 100);
+    }
+
+    if (response.method === "left") {
+        isGameActive = false;
+        messageElement.textContent = response.message;
+    }
+};
+
+cellElements.forEach((cell, index) => cell.addEventListener("click", event => {
+    makeMove(event.target, index)
+}));
+
+function makeMove(cell, index) {
+    if (!isGameActive || field[index] !== "") return;
+
+    isGameActive = false;
+    cell.classList.add(symbol);
+    field[index] = symbol;
+
+    ws.send(JSON.stringify({
+        "method": "move",
+        "symbol": symbol,
+        "field": field
+    }));
+}
+
+function updateBoard() {
+    cellElements.forEach((cell, index) => {
+        cell.classList.remove("X", "O");
+        field[index] !== "" && cell.classList.add(field[index]);
+    });
+}
+
+function updateMessage() {
+    if (symbol === turn) {
+        messageElement.textContent = "move";
+    } else {
+        messageElement.textContent = `weiting ${turn}...`;
+    }
+}
---
+++ server/server.js
@@ -0,0 +1,131 @@
+const express = require('express');
+const path = require('path');
+const http = require('http');
+const WebSocket = require('ws');
+
+const app = express();
+app.use(express.static(path.join(__dirname, "..", "client")));
+app.listen(3000);
+
+const httpServer = http.createServer();
+const wss = new WebSocket.Server({ server: httpServer });
+httpServer.listen(8080);
+
+const clientConnections = {};
+const opponents = {};
+let clientIdsWaitingMatch = [];
+
+wss.on("connection", connection => {
+    const clientId = createClientId()
+    clientConnections[clientId] = connection;
+
+    matchClients(clientId)
+
+    connection.on("message", message => {
+        const result = JSON.parse(message);
+        
+        if (result.method === "move") {
+            moveHandler(result, clientId)
+        }
+    })
+
+    connection.on("close", () => {
+        closeClient(connection, clientId)
+    })
+});
+
+function matchClients(clientId) {
+    clientIdsWaitingMatch.push(clientId);
+
+    if (clientIdsWaitingMatch.length < 2) return;
+
+    const firstClientId = clientIdsWaitingMatch.shift();
+    const secondClientId = clientIdsWaitingMatch.shift();
+
+    opponents[firstClientId] = secondClientId;
+    opponents[secondClientId] = firstClientId;
+
+    clientConnections[firstClientId].send(JSON.stringify({
+        method: "join",
+        symbol: "X",
+        turn: "X",
+    }));
+
+    clientConnections[secondClientId].send(JSON.stringify({
+        method: "join",
+        symbol: "O",
+        turn: "X",
+    }))
+}
+
+function moveHandler(result, clientId) {
+    const opponentClientId = opponents[clientId];
+
+    if (checkWin(result.field)) {
+        [clientId, opponentClientId].forEach(cId => {
+            clientConnections[cId].send(JSON.stringify({
+                method: "result",
+                message: `${result.symbol} win`,
+                field: result.field
+            }));
+        });
+        return;
+    }
+
+    if (checkDraw(result.field)) {
+        [clientId, opponentClientId].forEach(cId => {
+            clientConnections[cId].send(JSON.stringify({
+                method: "result",
+                message: "Draw",
+                field: result.field
+            }));
+        });
+        return;
+    }
+
+    [clientId, opponentClientId].forEach(cId => {
+        clientConnections[cId].send(JSON.stringify({
+            method: "update",
+            turn: result.symbol === "X" ? "O" : "X",
+            field: result.field
+        }));
+    });
+}
+
+function closeClient(connection, clientId) {
+    connection.close();
+    const isLeftUnmachedClient = clientIdsWaitingMatch.some(unmathedClientId => unmathedClientId === clientId);
+
+    if (isLeftUnmachedClient) {
+        clientIdsWaitingMatch = clientIdsWaitingMatch.filter(unmathedClientId => unmathedClientId !== clientId);
+    } else {
+        const opponentClientId = opponents[clientId];
+        clientConnections[opponentClientId].send(JSON.stringify({
+            method: "left",
+            message: "opponent left"
+        }))
+    }
+}
+
+const winingCombos = [
+    [0, 1, 2], [3, 4, 5], [6, 7, 8], // rows
+    [0, 3, 6], [1, 4, 7], [2, 5, 8], // columns
+    [0, 4, 8], [2, 4, 6]             // diagonals
+]
+
+function checkWin(field) {
+    return winingCombos.some(combo => {
+        const [first, second, third] = combo;
+        return field[first] !== "" && field[first] === field[second] && field[first] === field[third]
+    });
+}
+
+function checkDraw(field) {
+    return field.every(symbol => symbol === "X" || symbol === "O");
+}
+
+let clientIdCounter = 0;
+function createClientId() {
+    clientIdCounter++;
+    return clientIdCounter;
+}
